291a5e95289d022d609df78d36f1b90a7409057e
binhphung <binhphung18032000@gmail.com>
2022-06-30 01:09:30 +0700

    Updated price for minishop 300622 01:09 AM

--- Chapter01/ch01_shopping_mini/main.js
+++ Chapter01/ch01_shopping_mini/main.js
@@ -1,71 +1,44 @@
-var vueInstance = new Vue({
-    el: '#app',
-    data: {
-        title: 'Áo thun nam thể thao hàng VNXK vải dày mịn - Vải Đốm',
-        url: 'https://www.lazada.vn/products/ao-thun-nam-the-thao-hang-vnxk-vai-day-min-vai-dom-i265780948-s382816279.html',
-        target: '_blank',
-        price: 20000,
-        sale: 0.1,
-        selectedProduct: 2,
-        cardNumber: 1,
-        listProductDetail: [
-            {
-                image: './images/red.jpg',
-                quantity: 0,
-                textColor: 'Màu Đỏ'
-            }, {
-                image: './images/blue.jpg',
-                quantity: 8,
-                textColor: 'Màu Xanh'
-            }, {
-                image: './images/black.jpg',
-                quantity: 2,
-                textColor: 'Màu Đen'
-            }, 
-        ],
-        listDesc: [
-            'Chất liệu: polyester và thun',
-            'Thoát mồ hôi tốt',
-            'Áo thun cổ tròn thể thao Hiye chuyên được may từ chất liệu nilon thoáng mát',
-            'Kết hợp thêm sợi thun tạo độ co giãn giúp người tiêu dùng thoải mái khi mặc',
-            'Chất liệu: polyester và thun'
-        ],
-        description: '<p>M&ocirc; tả: &Aacute;o thun cổ tr&ograve;n thể thao Hiye chuy&ecirc;n được may từ chất liệu nilon tho&aacute;ng m&aacute;t v&agrave; tho&aacute;t mồ h&ocirc;i tốt, kết hợp th&ecirc;m sợi thun tạo độ co gi&atilde;n gi&uacute;p người ti&ecirc;u d&ugrave;ng thoải m&aacute;i khi mặc</p> ',
-        testDescription: `<p>Test hjskhfk h</p>
+const { createApp } = Vue;
 
-        <p><strong><span style="background-color:#8e44ad">fhsdjkfhsk&nbsp; hsdjkfk&nbsp;</span></strong></p>`
-    },
-    methods: {
-        handleClickColor(e, index) {
-            this.selectedProduct = index;
-            // console.log(e, index, this);
-        },
-        classActive(index) {
-            return {
-                active: this.selectedProduct === index
-            }
-        },
-        handleAddToCart(e) {
-            if(this.cardNumber + 1 > this.getProduct.quantity) {
-                alert('So luong khong du');
-            } else {
-                this.cardNumber = this.cardNumber + 1;
-            }
-            console.log(e.target)
-        }
+createApp({
+    data() {
+        return {
+            title: 'Áo bóng đá nam vải Mỹ nhập khẩu',
+            url: 'https://shopeefood.vn/ho-chi-minh/ta-la-an-vat-mi-xao-com-chien-sinh-to-phan-van-tri',
+            target: '_blank',
+            price: 100000,
+            sale: 0.5,
+            selectedProduct: 0,
+            listProductDetail: [
+                {
+                    image: './images/italia.jpg',
+                    quantity: 10,
+                    shirtColor: 'Màu xanh nước biển'
+                },
+                {
+                    image: './images/spain.jpg',
+                    quantity: 1,
+                    shirtColor: 'Màu đỏ máu lửa'
+                },
+                {
+                    image: './images/brazil.jpg',
+                    quantity: 0,
+                    shirtColor: 'Màu vàng gia tộc'
+                },
+            ]
+        };
     },
     computed: {
         formatOriginalPrice() {
-            var number = this.price;
-            return new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'VND' }).format(number);
+            var originalPrice = this.price;
+            return new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'VND' }).format(originalPrice);
         },
         formatFinalPrice() {
-            var number = this.price - this.sale * this.price;
-            return new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'VND' }).format(number);
+            var finalPrice = this.price - this.sale * this.price;
+            return new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'VND' }).format(finalPrice);
         },
-        getProduct() {
-            let index = this.selectedProduct;
-            return this.listProductDetail[index];
+        priceSaleOff() {
+            return this.sale * 100;
         }
     }
-});
+}).mount("#app");
